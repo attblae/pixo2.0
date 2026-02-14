12086147756b03c88e5d539febb8f186a873fa45
attblae <gnetovanadezda@gmail.com>
2026-02-14 12:20:43 +0300

    feat: how to check wether token is steal alive???

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse, JSONResponse
+from passlib.context import CryptContext
+from datetime import datetime, timedelta, time
+from jose import JWTError, jwt
 from pydantic import BaseModel
 import sqlite3
 import uvicorn
@@ -9,6 +12,11 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
+
+SECRET_KEY = "change-me-in-production-please"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 # classes:
 
 class Creating_user(BaseModel):
@@ -27,6 +35,16 @@ class Login(BaseModel):
     username: str
     password: str
 
+class Token(BaseModel):
+    access_token: str
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(password: str, hashed: str) -> bool:
+    return pwd_context.verify(password, hashed)
+
 # errors:
 
 @app.exception_handler(HTTPException)
@@ -39,6 +57,10 @@ async def http_exception_handler(request: Request, exc: HTTPException):
         }
     )
 
+def create_access_token(subject: str, expires_delta = None) -> str:
+    expire = time.time() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode = {"sub": subject, "exp": expire}
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 # pages:
 
@@ -78,25 +100,40 @@ def about_us_page():
 
 @app.post("/login_account")
 def login_account(data: Login):
-    if data.username != 'attblae':
-        raise HTTPException(detail='user does not exist', status_code=404)
-    return {"status": "ok"}
-    
-    
+    con = sqlite3.connect("base/tables.sql")
+    cursor = con.cursor()
+
+    hash_pass = cursor.execute("SELECT password FROM users WHERE username = ?", (data.username,)).fetchone()
+
+    if not hash_pass:
+        raise HTTPException(detail="User does not exists")
+
+    if not verify_password(data.password, hash_pass[0]):
+        raise HTTPException(detail="Invalid password", status_code=404)
+
+    con.close()
+    token = create_access_token(data.username)
+
+    response = Token(token)
+    return response
     
 
 @app.post("/create_user")
 def creating_user(data: Creating_user):
-    # data_information = data.model_dump()
-    # if any(" " in data_info for data_info in data_information.values()):
-    #     raise HTTPException(status_code=400, detail="Information contains blank space")
+    data_information = data.model_dump()
+    if any(" " in data_info for data_info in data_information.values()):
+        raise HTTPException(status_code=400, detail="Information contains blank space")
     
+    if data.phone.startswith("+7"):
+        data.phone = '8' + data.phone.removeprefix('+7')
+
     con = sqlite3.connect("base/tables.sql")
     cursor = con.cursor()
-    # username = cursor.execute("SELECT username FROM users WHERE username = ?", (data.username,)).fetchone()
 
-    # if username:
-    #     raise HTTPException(detail="username is already used", status_code=404)
+    username = cursor.execute("SELECT username FROM users WHERE username = ?", (data.username,)).fetchone()
+
+    if username:
+        raise HTTPException(detail="username is already used", status_code=404)
     
     if data.password_confirm != data.password:
         raise HTTPException(detail="password does not confirmed", status_code=404)
@@ -118,7 +155,7 @@ def creating_user(data: Creating_user):
         """,
             (
                 data.username,
-                data.password,
+                hash_password(data.password),
                 data.name,
                 data.surname,
                 data.patronymic,
@@ -131,6 +168,13 @@ def creating_user(data: Creating_user):
     con.commit()
     con.close()
     return {"status": "ok"}
+
+@app.post("/check_token")
+def check_for_token(token):
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    expires_at = payload["exp"]
+
+    return(expires_at)
 
 # sql part
 def create_tables(cursor):
