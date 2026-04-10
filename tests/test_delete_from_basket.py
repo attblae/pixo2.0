db6512ed35ec135c6eac41d7031ec596790d2e46
Гнетова Надежда Сергеевна <GnetovaNS@stud.rosnou.ru>
2026-04-10 16:45:05 +0300

    feat: first test of deliting from basket

--- tests/test_delete_from_basket.py
+++ tests/test_delete_from_basket.py
@@ -1,16 +1,18 @@
 import unittest
 
+from schemes.post_schemes import PostInfoSchema
 from service.posts_services import PostService
 from service.users_services import UserService
 
 
 class TestDeleteBasketPost(unittest.TestCase):
     def setUp(self):
-        class PostInfoSchema:
-            access_token: dict = UserService.create_access_token(
-                "example_name", expires_delta=None
-            )
-            link: str = "static/images/0.png"
+        self.data = PostInfoSchema(access_token=UserService.create_access_token(
+            "example_name",
+            expires_delta=None
+            ),
+            link="static/images/0.png"
+        )
 
     def test_delete_basket_post(self):
-        self.assertEqual(PostService.delete_basket_post(), "FOO")
+        self.assertEqual(PostService.delete_basket_post(self.data, username="example_name"), None)
